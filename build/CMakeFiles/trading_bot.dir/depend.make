4c668d07ad2ebb119013c12612699b970108d762
Bs1018007 <bs1018007@gmail.com>
2025-12-23 15:54:20 +0530

    fetch crypto

--- build/CMakeFiles/trading_bot.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for trading_bot.
-# This may be replaced when dependencies are built.
--- generated/trading/sbe/package-info.java
+++
@@ -1,5 +0,0 @@
-/* Generated SBE (Simple Binary Encoding) message codecs.*/
-/**
- * Trading Bot SBE Messages
- */
-package trading.sbe;
